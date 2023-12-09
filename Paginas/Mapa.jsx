636b3357a6f42d608c4e2c939a46848b5dfc9cf0
maximiliano745 <maximiliano745@gmail.com>
2023-12-09 20:30:44 -0300

    iconos que se mueven y se ponen

--- Paginas/Mapa.jsx
+++ Paginas/Mapa.jsx
@@ -6,21 +6,18 @@ import { IoWarningOutline } from "react-icons/io5";
 
 const Mapa = () => {
   const [iconCoords, setIconCoords] = useState({ x: 0, y: 0 });
+  const [droppedIcons, setDroppedIcons] = useState([]);
 
   let lon = -57.9023753;
   let lat = -34.8874779;
 
-
-
   useEffect(() => {
-    // Obtén las dimensiones del mapa al cargarse
     const mapDiv = document.querySelector('.leaflet-container');
     if (mapDiv) {
       const { top, right } = mapDiv.getBoundingClientRect();
       setIconCoords({ x: right, y: top });
     }
-  }, []); // Se ejecutará solo una vez al cargar la página
-
+  }, []);
 
   const iconsStyle = {
     position: 'fixed',
@@ -34,52 +31,67 @@ const Mapa = () => {
   };
 
   const iconContainerStyle = {
-    marginBottom: '10px', // Agrega un espacio entre los iconos
-    cursor: 'move', // Hace que el cursor se convierta en una mano para indicar que se pueden arrastrar
+    marginBottom: '10px',
+    cursor: 'move',
   };
 
-  const handleDrop = (event) => {
-    setIconCoords({ x: event.clientX, y: event.clientY });
-    alert(iconCoords.x, iconCoords.y);
+  const handleDrop = (event, draggedIcon) => {
+    const newIcon = {
+      icon: draggedIcon,
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    setDroppedIcons((prevIcons) => [...prevIcons, newIcon]);
+    alert(`${newIcon.icon.type.name} fue soltado en (${newIcon.x}, ${newIcon.y}) con el icono: (${newIcon.icon.type.name})`);
   };
 
   return (
     <div>
       <h3 style={{ display: 'flex', justifyContent: 'center' }}>Mis Lugares</h3>
-      <MapContainer style={{ height: "90%", width: "1300px" }}
-        center={[lat, lon]} zoom={10}
-      >
+      <MapContainer style={{ height: "90%", width: "1300px" }} center={[lat, lon]} zoom={10}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {/* Marcadores, etc., de react-leaflet */}
       </MapContainer>
 
-      {/* Contenedor de iconos */}
-
-
       <div style={iconsStyle}>
-      <div style={{ top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}>
-        <div
-          style={{ ...iconContainerStyle, top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}
-          draggable={true}
-          onDragEndCapture={handleDrop}
-        >
-          <AiFillWarning size={30} style={{ color: 'red', margin: '5px' }} />
-        </div>
+        <div style={{ top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}>
+          <div
+            style={{ ...iconContainerStyle, top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}
+            draggable
+            onDragEndCapture={(event) => handleDrop(event, <AiFillWarning size={30} style={{ color: 'red', margin: '5px' }} />)}
+          >
+            <AiFillWarning size={30} style={{ color: 'red', margin: '5px' }} />
+          </div>
         </div>
 
         <div style={{ top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}>
-        <div
-          style={{ ...iconContainerStyle, top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}
-          draggable={true}
-          onDragEndCapture={handleDrop}
-        >
-          <IoWarningOutline size={30} style={{ color: 'yellow', margin: '5px' }} />
-        </div>
+          <div
+            style={{ ...iconContainerStyle, top: `${iconCoords.y}px`, right: `${window.innerWidth - iconCoords.x}px` }}
+            draggable
+            onDragEndCapture={(event) => handleDrop(event, <IoWarningOutline size={30} style={{ color: 'yellow', margin: '5px' }} />)}
+          >
+            <IoWarningOutline size={30} style={{ color: 'yellow', margin: '5px' }} />
+          </div>
         </div>
       </div>
 
+      {droppedIcons.map((icon, index) => (
+        <div
+          key={index}
+          style={{
+            position: 'absolute',
+            left: `${icon.x}px`,
+            top: `${icon.y}px`,
+            zIndex: 9999,
+          }}
+          draggable
+          onDragStart={(e) => e.preventDefault()}
+        >
+          {icon.icon}
+        </div>
+      ))}
     </div>
   )
 }
